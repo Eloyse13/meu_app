f78b7c2123226bf748322e87fd3c6499bc8085e8
Eloyse <elo.becker.13@gmail.com>
2025-05-20 11:59:14 -0300

    primeiro comite

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Container(
+    child:  Text(
+      "Bem vindo ao Flutter",
+      textDirection: TextDirection.ltr,
+      style: TextStyle(color: Colors.yellow, fontSize: 30.0),
+    ),
+  ));
+}
+
+
+ 
